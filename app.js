1a1516606b13e6bd46896a8b55e691d3b0834ce4
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-06-23 14:24:09 -0500

    add style for popup window: mobile and desktop version

--- app.js
+++ app.js
@@ -154,51 +154,72 @@ const popContainer = document.createElement('div');
 popContainer.classList.add('pop-window');
 sectionProjects.appendChild(popContainer);
 
-const popTitle = document.createElement('h2')
+// Title and closing cross //
+const headerPop = document.createElement('div');
+headerPop.classList.add('pop-header');
+popContainer.appendChild(headerPop);
+
+const popTitle = document.createElement('h2');
 popTitle.classList.add('pop-title');
 popTitle.textContent = "Tonic";
-popContainer.appendChild(popTitle);
+headerPop.appendChild(popTitle);
 
-const rowList = document.createElement('div');
-rowList.classList.add('list');
-popContainer.appendChild(rowList);
+const closePop = document.createElement('button');
+closePop.classList.add('close-cross');
+closePop.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+headerPop.appendChild(closePop);
 
+// Tags: client, role, year
 const popList = document.createElement('ul');
-popList.classList.add('list-pop');
-rowList.appendChild(popList);
+popList.classList.add('list');
+popContainer.appendChild(popList);
 
 const elemt1 = document.createElement('li');
 elemt1.classList.add('canopy');
-elemt1.textContent = "CANOPY";
+elemt1.textContent = 'CANOPY';
 popList.appendChild(elemt1);
 
 const elemt2 = document.createElement('li');
 elemt2.classList.add('back-end');
-elemt2.textContent = "Back End Dev";
+elemt2.textContent = 'Back End Dev';
 popList.appendChild(elemt2);
 
 const elemt3 = document.createElement('li');
 elemt3.classList.add('num');
-elemt3.textContent = "2015";
-popList.appendChild(elemt2);
+elemt3.textContent = '2015';
+popList.appendChild(elemt3);
 
+// Project image //
 const imgPop = document.createElement('img');
 imgPop.classList.add('img-pop');
 imgPop.src = "./images/Snapshoot.png";
 imgPop.alt = "Snaoshoot"
 popContainer.appendChild(imgPop);
 
+// Media query container//
+
+const mediaQuery = document.createElement('div');
+mediaQuery.classList.add('query-container');
+popContainer.appendChild(mediaQuery);
+
+// Project descripcion //
 const textPop = document.createElement('div');
 textPop.classList.add('information');
-popContainer.appendChild(textPop);
+mediaQuery.appendChild(textPop);
 
 const pPop = document.createElement('p');
 pPop.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent";
 textPop.appendChild(pPop);
 
+// media query sub-container //
+const subContainer = document.createElement('div');
+subContainer.classList.add('sub-container');
+mediaQuery.appendChild(subContainer);
+
+// Languages cards //
 const formPop = document.createElement('form');
-formPop.classList.add('buttom');
-popContainer.appendChild('formPop');
+formPop.classList.add('buttom-pop');
+subContainer.appendChild(formPop);
 
 const buttonPop = document.createElement('button');
 buttonPop.classList.add('html');
@@ -218,9 +239,10 @@ buttonPop3.type = "button";
 buttonPop3.textContent = "javaScript";
 formPop.appendChild(buttonPop3);
 
+// Link buttons //
 const divButton = document.createElement('div');
 divButton.classList.add('popbutton');
-popContainer.appendChild(divButton);
+subContainer.appendChild(divButton);
 
 const liveButton = document.createElement('button');
 liveButton.classList.add('live');
